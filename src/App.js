313b6bcaf13614d9cc12cadac04b02229882ba63
Ross Keenan <rkeenan@auroracoop.com>
2020-11-04 16:40:51 -0600

    Added in Bootsrap.css

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import Button from './Components/Button';
-import TextInput from './Components/TextInput';
+
 
 function App() {
   return (
     <div className="App">
-
-    
-
+      
     </div>
   );
 }
