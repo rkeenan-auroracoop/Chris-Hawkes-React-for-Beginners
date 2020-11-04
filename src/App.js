81ac68c6e352e7f77446ace6ea6ae273f98ff9a2
Ross Keenan <rkeenan@auroracoop.com>
2020-11-04 16:49:15 -0600

    Removed index.css and import for index.css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 
-
 function App() {
   return (
     <div className="App">
-      
+      <React.Fragment>
+        <div className="container ">
+          <div className="row">
+            <h1 className="">My Videos</h1>
+          </div>
+        </div>
+      </React.Fragment>
     </div>
   );
 }
