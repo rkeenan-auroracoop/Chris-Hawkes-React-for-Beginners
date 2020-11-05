d2bcbfd34a524b4d3082ce1a5847b6bcac32c22a
Ross Keenan <rkeenan@auroracoop.com>
2020-11-05 11:05:59 -0600

    Added Video component with styling

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Video from './Components/Video'
+
 
 function App() {
   return (
@@ -7,6 +9,19 @@ function App() {
         <div className="container ">
           <div className="row">
             <h1 className="text-center">My Videos</h1>
+            <div className="col-md-6">
+              <Video 
+              url="https://www.youtube.com/embed/SMQOv_NT6WE"
+              title="Things I Wish I Knew When I Started Programming"
+              />
+
+            </div>
+            <div className="col-md-6">
+              <Video 
+              url="https://www.youtube.com/embed/YVDLNFEuvEI"
+              title="Orderly Dissolution of the United States into Its Component Parts"
+              />
+            </div>
           </div>
         </div>
       </React.Fragment>
